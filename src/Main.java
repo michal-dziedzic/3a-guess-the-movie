38968c20e52a7990a3ae1f047499153091bc2591
Michal Dziedzic <2dziedzic.michal@gmail.com>
2022-06-12 18:22:21 +0200

    second scanner added for movie title loading

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("movies.txt");
         Scanner fileScanner = new Scanner(file);
-        
+        Scanner fileScanner2 = new Scanner(file);
+        int randomMovie =chooseRandomMovie(movieTitleCount(fileScanner));
         //line.toCharArray()
-        System.out.println(chooseRandomMovie(movieTitleCount(fileScanner)));
+        System.out.println(randomMovie);
+        System.out.println(loadMovieTitle(fileScanner2, randomMovie));
 
 
     }
@@ -16,14 +18,30 @@ public class Main {
     private static int movieTitleCount (Scanner fileScanner) {
         int numbersOfLinesInFile = 0;
         while (fileScanner.hasNextLine()) {
-            String line =fileScanner.nextLine();
+            fileScanner.nextLine();
             numbersOfLinesInFile++;
         }
         return numbersOfLinesInFile;
+    }
+
+    private static String loadMovieTitle (Scanner fileScanner, int randomMovie){
+        int lineNumber = 0;
+        String movieTitle = null;
+        while (fileScanner.hasNextLine()) {
+            movieTitle = fileScanner.nextLine();
+            if (lineNumber == randomMovie) {
+                break;
+            }
+            lineNumber++;
+
+        }
+        return movieTitle;
     }
 
     private static int chooseRandomMovie (int movieTitleCount) {
         System.out.println(movieTitleCount);
         return (int) (Math.random() * movieTitleCount);
     }
+
+
 }
