4b7305bd675c322eb7cec596ad74b85c26cb6895
Michal Dziedzic <2dziedzic.michal@gmail.com>
2022-06-12 16:51:54 +0200

    movie list added and random picks of movie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("movies.txt");
+        Scanner fileScanner = new Scanner(file);
+        
+        //line.toCharArray()
+        System.out.println(chooseRandomMovie(movieTitleCount(fileScanner)));
+
+
+    }
+
+    private static int movieTitleCount (Scanner fileScanner) {
+        int numbersOfLinesInFile = 0;
+        while (fileScanner.hasNextLine()) {
+            String line =fileScanner.nextLine();
+            numbersOfLinesInFile++;
+        }
+        return numbersOfLinesInFile;
+    }
+
+    private static int chooseRandomMovie (int movieTitleCount) {
+        System.out.println(movieTitleCount);
+        return (int) (Math.random() * movieTitleCount);
     }
 }
